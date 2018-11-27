5f9bdd4606b725b7e7530b195c2f7583f81bf3c8
Martijn Vegter <mail@mvegter.me>
2018-11-27 13:41:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 /* Dependencies */
-const helmet = require('helmet');
-
 const express = require('express');
+const helmet = require('helmet');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const controllers = require('./controllers');
 
 const app = express();
@@ -17,11 +15,22 @@ app.use(helmet());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+/* HTTP request logger middleware for node.js */
 app.use(logger('dev'));
+
+/* It parses incoming requests with JSON payloads. */
 app.use(express.json());
+
+/* It parses incoming requests with urlencoded payloads. */
 app.use(express.urlencoded({ extended: false }));
+
+/* Parse Cookie header and populate req.cookies with an object keyed by the cookie names. */
 app.use(cookieParser());
+
+/* It serves static files and is based on serve-static. */
 app.use(express.static(path.join(__dirname, 'public')));
+
+/* Controllers */
 app.use(controllers);
 
 module.exports = app;
