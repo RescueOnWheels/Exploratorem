186102309d8ffdd14ff518900abdc130352e8172
Raphaël Bunck <raphael@bunck.me>
2018-12-07 14:56:02 +0100

    Added some LGBT rainbow flags

--- public/terminal.js
+++ public/terminal.js
@@ -1,10 +1,48 @@
+/**
+ * Function for adding effects and color to your terminal string
+ * @param {*} col The color or effect you want to add to your string
+ * @param {String} string The string you are trying to add effects to
+ */
+function ansi(col, string) {
+  switch (col) {
+    case 'red':
+      return `\x1b[31m${string}\x1b[0m`;
+    case 'green':
+      return `\x1b[32m${string}\x1b[0m`;
+    case 'yellow':
+      return `\x1b[33m${string}\x1b[0m`;
+    case 'blue':
+      return `\x1b[34m${string}\x1b[0m`;
+    case 'magenta':
+      return `\x1b[35m${string}\x1b[0m`;
+    case 'cyan':
+      return `\x1b[36m${string}\x1b[0m`;
+    case 'bold':
+      return `\x1b[1m${string}\x1b[0m`;
+    case 'italic':
+      return `\x1b[3m${string}\x1b[0m`;
+    case 'underline':
+      return `\x1b[4m${string}\x1b[0m`;
+    case 'strike':
+      return `\x1b[9m${string}\x1b[0m`;
+    case 'inverse':
+      return `\x1b[7m${string}\x1b[0m`;
+    default:
+      break;
+  }
+}
+
 // eslint-disable-next-line no-undef
 const term = new Terminal();
 term.prompt = () => {
   term.write('\r\n$ ');
 };
 term.open(document.getElementById('terminal'));
-term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
+const now = new Date();
+term.writeln(now);
+term.writeln(`Welcome to ${ansi('magenta', 'dora')}`);
+
+term.writeln(ansi('blue', 'color test 123'));
 term.prompt();
 
 // The theme settings and color settings
