a6b621706c68518e4d021904a6b317bd0bd09da4
Raphaël Bunck <raphael@bunck.me>
2019-01-07 09:36:07 +0100

    asdf

--- controllers/io.controller.js
+++ controllers/io.controller.js
@@ -5,7 +5,7 @@ const io = require('socket.io')();
  */
 io.on('connection', (socket) => {
   socket.emit('news', { hello: 'world' });
-  socket.on('my other event', (data) => {
+  // socket.on('my other event', (data) => {
     // console.log(data);
   });
   socket.on('disconnect', () => {
